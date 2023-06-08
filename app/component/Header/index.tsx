43c9b6f3096f46e76a5d00f25bd2d238789f0301
yunjeoming <yunjeoming@gmail.com>
2023-06-08 10:19:13 +0900

    fix: darkmode error

--- app/component/Header/index.tsx
+++ app/component/Header/index.tsx
@@ -5,26 +5,22 @@ import MainHeader from './MainHeader';
 import SubHeader from './SubHeader';
 import { Theme } from '@/types/Blog';
 
-export const Header: React.FC = () => {
-  const firstTheme = useMemo(() => {
-    if (typeof document === 'undefined') return 'light';
-    return document.cookie.split(';')[0].split('=')[1] as Theme;
-  }, []);
-  const [theme, setTheme] = useState<Theme>(firstTheme === 'light' ? 'light' : 'dark');
+interface Props {
+  theme: string;
+}
+
+export const Header: React.FC<Props> = ({ theme: cookieTheme }) => {
+  const [theme, setTheme] = useState<Theme>(cookieTheme as Theme);
 
   const changeTheme = useCallback(() => {
+    document.documentElement.classList.toggle('dark');
     setTheme((state) => (state === 'dark' ? 'light' : 'dark'));
-  }, []);
-
-  useEffect(() => {
-    if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark');
-      document.cookie = 'theme=dark; max-age=10000';
+    if (theme === 'dark') {
+      document.cookie = 'theme=light;';
     } else {
-      document.documentElement.classList.remove('dark');
-      document.cookie = 'theme=light; max-age=10000';
+      document.cookie = 'theme=dark;';
     }
-  }, [theme]);
+  }, []);
 
   return (
     <>
