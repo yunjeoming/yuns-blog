e5de2dba8143f9505fad2038b2834f69a57c2125
yunjeoming <yunjeoming@gmail.com>
2023-08-02 21:19:48 +0900

    delete: delete SelectedTag

--- app/component/Tag/Tags.tsx
+++ app/component/Tag/Tags.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { SelectedTag, Tag } from './';
+import { Tag } from './';
 import { ChevronUp, ChevronDown } from 'react-feather';
 import { PostPage } from '@/src/types/Common';
 import useMoreAndHide from '@/src/hooks/useMoreAndHide';
@@ -19,13 +19,9 @@ const Tags: React.FC<Props> = ({ tags, selectedTag, pageName }) => {
     <aside className="flex-grow-0 flex-shrink-0 w-full lg:w-auto lg:basis-60 lg:py-4 lg:px-2 lg:mr-6 lg:h-fit max-lg:mb-8">
       <div className="mb-2 lg:mb-4 dark:text-tag-dark">Tag</div>
       <div className="flex flex-wrap overflow-hidden gap-2 max-h-7 sm:max-h-8 lg:max-h-[14rem]" ref={tagsRef}>
-        {tags.map((t) =>
-          t == selectedTag ? (
-            <SelectedTag key={t} name={t} pageName={pageName} />
-          ) : (
-            <Tag key={t} name={t} pageName={pageName} />
-          ),
-        )}
+        {tags.map((t) => (
+          <Tag key={t} name={t} pageName={pageName} selected={t === selectedTag} />
+        ))}
       </div>
       {isOverHeight ? (
         <button className="flex justify-center items-center w-full p-2 text-sm sm:text-base" onClick={handleClick}>
