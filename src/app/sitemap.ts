8b209ce2a61c25171553e9ca9db7de17295339d4
yunjeoming <yunjeoming@gmail.com>
2023-09-23 10:56:57 +0900

    fix: remove root url for SEO

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,9 +5,8 @@ import { PostUtil } from '@/utils/post';
 export default function sitemap(): MetadataRoute.Sitemap {
   const blogPosts = PostUtil.getAllBlogPosts();
   const issuePosts = PostUtil.getAllIssuePosts();
-  const navUrl = [Constants.BLOG_PATH, Constants.ISSUE_PATH];
   const slugs = PostUtil.getPostMetasByPosts([...blogPosts, ...issuePosts]).map((p) => p.slug);
-  const urls = [...navUrl, ...slugs].map((url) => {
+  const urls = [...slugs].map((url) => {
     return {
       url: `${Constants.URL}/${url}`,
     };
