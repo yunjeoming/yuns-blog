66542da47dfb8cc4fab55d3850ac08594d22b4dd
yunjeoming <yunjeoming@gmail.com>
2023-10-04 22:12:11 +0900

    refactor: add category, allPostMetas and delete issue posts in post util

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,31 +1,20 @@
 import { Metadata } from 'next';
-import { IPostMeta, IPost } from '@/types/post';
+import { IPostMeta, IPost, ICategory } from '@/types/post';
 import { allPosts, Post } from 'contentlayer/generated';
-import { compareDesc } from 'date-fns';
 import { Constants } from '@/constants';
 
-let posts = allPosts.filter(p => p.isPublished);
+let posts = allPosts.filter((p) => p.isPublished);
 let blogPosts: Post[] | null = null;
 let memoPosts: Post[] | null = null;
-let issuePosts: Post[] | null = null;
-let serieses: Post[] | null = null;
+let categories: ICategory | null = null;
 let blogTags: string[] | null = null;
-let issueTags: string[] | null = null;
+let allPostMetas: IPostMeta[] = [];
 
 export const PostUtil = {
   /**
-   * 모든 게시물 불러오기
-   * @returns posts: Post[]
-   */
-  getAllPosts: () => {
-    return posts;
-  },
-
-  /**
    * 모든 블로그 게시물 불러오기
-   * @returns posts: Post[]
    */
-  getAllBlogPosts: () => {
+  getAllBlogPosts() {
     if (!blogPosts) {
       blogPosts = posts.filter((post) => post.url.startsWith(Constants.BLOG_PATH));
     }
@@ -34,9 +23,8 @@ export const PostUtil = {
 
   /**
    * 모든 메모 게시물 불러오기
-   * @returns posts: Post[]
    */
-  getAllMemoPosts: () => {
+  getAllMemoPosts() {
     if (!memoPosts) {
       memoPosts = posts.filter((post) => post.url.startsWith(Constants.MEMO_PATH));
     }
@@ -44,46 +32,83 @@ export const PostUtil = {
   },
 
   /**
-   * 모든 에러 게시물 불러오기
-   * @returns posts: Post[]
+   * 모든 블로그 게시물의 메타정보 불러오기
    */
-  getAllIssuePosts: () => {
-    if (!issuePosts) {
-      issuePosts = posts.filter((post) => post.url.startsWith(Constants.ISSUE_PATH));
+  getAllPostMetas() {
+    if (!allPostMetas.length) {
+      const blogPosts = this.getAllBlogPosts();
+      allPostMetas = this.getPostMetasByPosts(blogPosts);
     }
-    return issuePosts;
+    return allPostMetas;
+  },
+
+  /**
+   * 모든 카테고리 불러오기
+   */
+  getAllCategories() {
+    if (!categories) {
+      categories = this.getCategoryInfo().categories;
+    }
+    return categories;
   },
 
   /**
    * 모든 블로그 태그 불러오기
-   * @returns tags: string[]
    */
-  getAllBlogTags: () => {
+  getAllBlogTags() {
     if (!blogTags) {
-      const blogPosts = PostUtil.getAllBlogPosts();
-      blogTags = PostUtil.getAllTagsByPosts(blogPosts);
+      const blogPosts = this.getAllBlogPosts();
+      blogTags = this.getAllTagsByPosts(blogPosts);
     }
     return blogTags;
   },
 
   /**
-   * 모든 에러 태그 불러오기
-   * @returns tags: string[]
+   * 태그에 해당하는 블로그 게시물들의 메타 정보 가져오기
+   * @param tag 태그이름
    */
-  getAllIssueTags: () => {
-    if (!issueTags) {
-      const issuePosts = PostUtil.getAllIssuePosts();
-      issueTags = PostUtil.getAllTagsByPosts(issuePosts);
+  getBlogPostMetasByTag(tag: string) {
+    return this.getAllBlogPosts()
+      .filter((post) => post.tags?.includes(tag))
+      .map((post) => this.getPostMetaByOnePost(post));
+  },
+
+  /**
+   * 카테고리 정보 가져오기
+   */
+  getCategoryInfo() {
+    if (categories && allPostMetas.length) {
+      return { categories, allPostMetas };
     }
-    return issueTags;
+
+    const categoryMap = {} as ICategory;
+    const postMetas = [];
+
+    const blogPosts = this.getAllBlogPosts();
+    for (let post of blogPosts) {
+      const postMeta = this.getPostMetaByOnePost(post);
+      postMetas.push(postMeta);
+
+      if (post.category) {
+        const category = post.category;
+        const upperedCategory = category.charAt(0).toUpperCase() + category.slice(1);
+        categoryMap[upperedCategory] = categoryMap[upperedCategory]
+          ? [...categoryMap[upperedCategory], postMeta]
+          : [postMeta];
+      }
+    }
+
+    return {
+      categories: categoryMap,
+      allPostMetas: postMetas,
+    };
   },
 
   /**
    * slug으로 하나의 게시물(contentlayer의 Post) 가져옴
    * @param slug 파일명에서 확장자 제거된 이름(*)
-   * @returns post: Post
    */
-  getOriginPostBySlug: (slug: string) => {
+  getOriginPostBySlug(slug: string) {
     const decodedSlug = decodeURIComponent(slug);
     return posts.filter((post) => post._raw.flattenedPath.split('/')[1] === decodedSlug)[0];
   },
@@ -91,23 +116,22 @@ export const PostUtil = {
   /**
    * slug으로 하나의 게시물(게시물의 IPost) 가져옴
    * @param slug 파일명에서 확장자 제거된 이름(*)
-   * @returns post: IPost
    */
-  getIPostBySlug: (slug: string) => {
+  getIPostBySlug(slug: string) {
     const decodedSlug = decodeURIComponent(slug);
-    const post = PostUtil.getOriginPostBySlug(decodedSlug);
-    return PostUtil.convertPostTypeByPost(post);
+    const post = this.getOriginPostBySlug(decodedSlug);
+    return this.convertPostTypeByPost(post);
   },
 
   /**
    * 특정 게시물의 메타 정보
    * @param post 특정 게시물
-   * @returns postMeta: IPostMeta
    */
-  getPostMetaByOnePost: (post: Post) => {
+  getPostMetaByOnePost(post: Post) {
     const {
       title,
       description,
+      category,
       tags,
       date,
       imgUrl,
@@ -117,6 +141,7 @@ export const PostUtil = {
     return {
       title,
       description,
+      category,
       tags,
       date,
       imgUrl: `/posts/thumbnail/${imgUrl || 'default-thumbnail.jpeg'}`,
@@ -127,9 +152,8 @@ export const PostUtil = {
   /**
    * 특정 게시물의 메타 데이터 정보
    * @param post 특정 게시물
-   * @returns metadata: Metadata
    */
-  getMetadataBySlug: (slug: string): Metadata => {
+  getMetadataBySlug(slug: string): Metadata {
     const post = PostUtil.getOriginPostBySlug(slug);
     const {
       title,
@@ -173,81 +197,51 @@ export const PostUtil = {
 
   /**
    * 특정 게시물들의 메타 정보가 담긴 배열
-   * @returns postMetas: IPostMeta[]
    */
-  getPostMetasByPosts: (posts: Post[]) => {
-    const postMetas = posts.map((post) => PostUtil.getPostMetaByOnePost(post));
-    return PostUtil.compareDescDateByPostMetas(postMetas);
+  getPostMetasByPosts(posts: Post[]) {
+    return posts.map((post) => this.getPostMetaByOnePost(post));
   },
 
   /**
-   * 태그에 해당하는 블로그 게시물들의 메타 정보 가져오기
-   * @param tag 태그이름
-   * @returns postMetas: IPostMeta[]
+   * 카테고리에 해당하는 블로그 게시물들의 메타 정보 가져오기
+   * @param category 카테고리
    */
-  getBlogPostMetasByTag: (tag: string) => {
-    const tagPosts = PostUtil.getAllBlogPosts()
-      .filter((post) => post.tags?.includes(tag))
-      .map((post) => PostUtil.getPostMetaByOnePost(post));
-    return PostUtil.compareDescDateByPostMetas(tagPosts);
-  },
-
-  /**
-   * 태그에 해당하는 에러 게시물들의 메타 정보 가져오기
-   * @param tag 태그이름
-   * @returns postMetas: IPostMeta[]
-   */
-  getIssuePostMetasByTag: (tag: string) => {
-    const tagPosts = PostUtil.getAllIssuePosts()
-      .filter((post) => post.tags?.includes(tag))
-      .map((post) => PostUtil.getPostMetaByOnePost(post));
-    return PostUtil.compareDescDateByPostMetas(tagPosts);
+  getBlogPostMetasByCategory(category: string) {
+    const categories = this.getAllCategories();
+    return categories?.[category] || [];
   },
 
   /**
    * 특정 게시물들에 작성된 모든 tags 가져오기
-   * @returns tags: string[]
    */
-  getAllTagsByPosts: (posts: Post[]) => {
+  getAllTagsByPosts(posts: Post[]) {
     const tagsSet = new Set<string>();
     for (let post of posts) {
       post.tags?.forEach((tag) => {
         tagsSet.add(tag);
       });
     }
-    const tags = Array.from(tagsSet);
-    return ['ALL', ...tags];
+    return Array.from(tagsSet);
   },
 
   /**
    * contentlayer에서 제공하는 기존 Post 타입을 블로그 IPost 타입으로 변환
    * @param post 기존 Post 타입의 게시글
-   * @return post: IPost
    */
-  convertPostsTypeByPosts: (posts: Post[]) => {
-    return posts.map((post) => PostUtil.convertPostTypeByPost(post));
+  convertPostsTypeByPosts(posts: Post[]) {
+    return posts.map((post) => this.convertPostTypeByPost(post));
   },
 
   /**
    * contentlayer에서 제공하는 기존 Post 타입을 프로젝트의 IPost 타입으로 변환
    * @param post 기존 Post 타입의 게시글
-   * @return post: IPost
    */
-  convertPostTypeByPost: (post: Post) => {
-    const meta = PostUtil.getPostMetaByOnePost(post);
+  convertPostTypeByPost(post: Post) {
+    const meta = this.getPostMetaByOnePost(post);
     const content = post.body.code;
     return {
       meta,
       content,
     } as IPost;
-  },
-
-  /**
-   * 특정 메타 정보들을 날짜 내림차순으로 정렬
-   * @param postMetas 특정 메타정보의 배열
-   * @returns postMetas: IPostMeta[]
-   */
-  compareDescDateByPostMetas: (postMetas: IPostMeta[]) => {
-    return postMetas.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
   },
 };
